0e0a801391245bbdb672a13ea905dca10a69eaa4
Yukari-kyun <risserusaiki@gmail.com>
2026-03-24 13:12:21 +0800

    Training file for modular architecture

---
+++ Training.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
